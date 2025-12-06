aa97679b73155d42bbab7d1393dc4b6f671dc96d
username-xboybx <jeswanth012@gmail.com>
2025-12-06 14:11:19 +0530

    added env variables to client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,51 +1,54 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
+export default defineConfig(async ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      react(),
+      runtimeErrorOverlay(),
+      ...(process.env.NODE_ENV !== "production" &&
       process.env.REPL_ID !== undefined
-      ? [
-        await import("@replit/vite-plugin-cartographer").then((m) =>
-          m.cartographer(),
-        ),
-        await import("@replit/vite-plugin-dev-banner").then((m) =>
-          m.devBanner(),
-        ),
-      ]
-      : []),
-  ],
-  optimizeDeps: {
-    include: ["zod"],
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(process.cwd(), "src"),
-      "@shared": path.resolve(process.cwd(), "src/shared"),
-      "zod": path.resolve(process.cwd(), "node_modules/zod"),
+        ? [
+            await import("@replit/vite-plugin-cartographer").then((m) =>
+              m.cartographer(),
+            ),
+            await import("@replit/vite-plugin-dev-banner").then((m) =>
+              m.devBanner(),
+            ),
+          ]
+        : []),
+    ],
+    optimizeDeps: {
+      include: ["zod"],
     },
-  },
-  build: {
-    outDir: path.resolve(process.cwd(), "dist"),
-    emptyOutDir: true,
-  },
-  server: {
-    port: 5173,
-    host: "0.0.0.0",
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:5000",
-        changeOrigin: true,
-        secure: false,
+    resolve: {
+      alias: {
+        "@": path.resolve(process.cwd(), "src"),
+        "@shared": path.resolve(process.cwd(), "src/shared"),
+        "zod": path.resolve(process.cwd(), "node_modules/zod"),
       },
     },
-    fs: {
-      strict: true,
-      deny: ["**/.*"],
+    build: {
+      outDir: path.resolve(process.cwd(), "dist"),
+      emptyOutDir: true,
     },
-  },
+    server: {
+      port: 5173,
+      host: "0.0.0.0",
+      proxy: {
+        "/api": {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+      fs: {
+        strict: true,
+        deny: ["**/.*"],
+      },
+    },
+  };
 });
